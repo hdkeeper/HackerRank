d4e24b7208e9627e983926853ef0b857e4174948
Keeper <hd_keeper@mail.ru>
2019-09-07 18:41:46 +0300

    Common child

---
+++ strings/common-child.py
@@ -0,0 +1,17 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+def commonChild(a, b):
+    lengths = [[0 for j in range(len(b)+1)] for i in range(len(a)+1)]
+    for i, x in enumerate(a):
+        for j, y in enumerate(b):
+            if x == y:
+                lengths[i+1][j+1] = lengths[i][j] + 1
+            else:
+                lengths[i+1][j+1] = max(lengths[i+1][j], lengths[i][j+1])
+ 
+    return lengths[-1][-1]
+
+s1 = input()
+s2 = input()
+result = commonChild(s1, s2)
+print(result)
